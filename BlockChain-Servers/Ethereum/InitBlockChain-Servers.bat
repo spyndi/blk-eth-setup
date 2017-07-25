5f64a7ec31d02e4b09ce09168a5fffececf14935
spyndi <sudhakar06@gmail.com>
2017-07-25 11:52:55 -0400

    Add files via upload

---
+++ BlockChain-Servers/Ethereum/InitBlockChain-Servers.bat
@@ -0,0 +1,5 @@
+prompt Initializing Block Chain Server 1
+d:\BlockChain-Servers\Ethereum\bin\geth --datadir "d:\BlockChain-Servers\Ethereum\blockchain-1" init "d:\BlockChain-Servers\Ethereum\genesis.json"
+pause
+prompt Initializing Block Chain Server 2
+d:\BlockChain-Servers\Ethereum\bin\geth --datadir "d:\BlockChain-Servers\Ethereum\blockchain-2" init "d:\BlockChain-Servers\Ethereum\genesis.json"
